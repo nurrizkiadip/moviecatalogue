c5395fc44bb1a983b3f2790f4616d28794c00147
nurrizkiadip <66776057+nurrizkiadip@users.noreply.github.com>
2021-06-15 16:50:24 +0700

    Finish leakCanary and Security

--- core/src/main/java/com/nurrizkiadip_a1201541/moviecatalogue/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/nurrizkiadip_a1201541/moviecatalogue/core/data/source/remote/network/ApiService.kt
@@ -2,11 +2,8 @@ package com.nurrizkiadip_a1201541.moviecatalogue.core.data.source.remote.network
 
 import com.nurrizkiadip_a1201541.moviecatalogue.core.BuildConfig.API_KEY
 import com.nurrizkiadip_a1201541.moviecatalogue.core.data.source.remote.response.MovieResponse
-import com.nurrizkiadip_a1201541.moviecatalogue.core.data.source.remote.response.MovieDetailResponse
-import com.nurrizkiadip_a1201541.moviecatalogue.core.data.source.remote.response.TvDetailResponse
 import com.nurrizkiadip_a1201541.moviecatalogue.core.data.source.remote.response.TvShowResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
